568a7a8e63a0dc589f60cdfe28950a59b9db654d
robertvlad <vladrob96@gmail.com>
2023-05-04 16:40:08 +0300

    Bonus done

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -18,7 +18,21 @@ public class CibiPreferiti {
 		
 		System.out.println(cibiPreferiti[cibiPreferiti.length - 1]);
 		
+		// 4 - bonus;
 		
+		if (cibiPreferiti.length % 2 == 0) {
+			
+			System.out.println("I cibi in mezzo alla lista sono 2 e sono:");
+			System.out.println(cibiPreferiti[cibiPreferiti.length / 2 - 1]);
+			System.out.println(cibiPreferiti[cibiPreferiti.length / 2]);
+			
+		}
+		else {
+			
+			System.out.println("Il cibo in mezzo alla classifica è:");
+			System.out.println(cibiPreferiti[cibiPreferiti.length / 2]);
+			
+		}
 		
 	}
 }
