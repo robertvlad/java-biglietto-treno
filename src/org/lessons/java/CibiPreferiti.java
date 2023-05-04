d24c7c62bef71a491a754f014cb099f9dca6396d
robertvlad <vladrob96@gmail.com>
2023-05-04 16:32:21 +0300

    Base exercise completed

--- src/org/lessons/java/CibiPreferiti.java
+++ src/org/lessons/java/CibiPreferiti.java
@@ -1,5 +1,24 @@
 package org.lessons.java;
 
 public class CibiPreferiti {
-
+	
+	public static void main(String[] args) {
+		
+		String[] cibiPreferiti = {"patatine", "brodo", "bistecca", "frutti", "panino"};
+		
+		// 1 - lunghezza array;
+		
+		System.out.println(cibiPreferiti.length);
+		
+		// 2 - cibo preferito (prima posizione);
+		
+		System.out.println(cibiPreferiti[0]);
+		
+		// 3 - cibo ultimo in classifica;
+		
+		System.out.println(cibiPreferiti[cibiPreferiti.length - 1]);
+		
+		
+		
+	}
 }
